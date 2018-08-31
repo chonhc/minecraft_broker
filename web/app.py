170378ef666480501a3995797bcfe66d550f3ca5
Adam Mendes <chon.hc@gmail.com>
2018-08-31 12:31:51 -0300

    Adding create instance

--- web/app.py
+++ web/app.py
@@ -1,11 +1,43 @@
-import requests
 from flask import Flask, request
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
+from os import path
+from pprint import pprint
+
+import kubernetes
+import requests
+import time
+import yaml
 
 app = Flask(__name__)
 control_instance_number = 0
+POD_YAML_FILE = "minecraft-server-pod.yaml"
+SERVICE_YAML_FILE = "minecraft-server-service.yaml"
+NAMESPACE = "default"
+PRETTY = 'pretty_example'
 
-def store_instance(instance):
-    return 'Store {}'.format(instance)
+def store_instance():
+    config.load_kube_config()
+    podspec = {}
+    servicespec = {}
+
+    with(open(path.join("yaml", POD_YAML_FILE))) as podfile:
+        podspec = yaml.load(podfile)
+        print("Pod spec is:" + str(podspec))
+
+    with(open(path.join("yaml", SERVICE_YAML_FILE))) as servicefile:
+        servicespec = yaml.load(servicefile)
+        print("Service spec is:" + str(servicespec))
+    try:
+        k8sapi = client.CoreV1Api()
+        identity = str(int(time.time()))
+        podspec["metadata"]["name"] = podspec["metadata"]["name"] + "-" + identity
+        k8sapi.create_namespaced_pod(namespace=NAMESPACE, body=podspec, pretty=PRETTY)
+        servicespec["metadata"]["name"] = servicespec["metadata"]["name"] + "-" + identity
+        k8sapi.create_namespaced_service(namespace=NAMESPACE, body=servicespec, pretty=PRETTY)
+    except ApiException as e:
+        print("Exception when creating pod: %s\n" % e)
+    return ('', 200)
 
 def get_instances():
     return 'List of Instances'
@@ -20,16 +52,16 @@ def delete_instances(instance):
 def hello_world():
     return 'Flask Dockerized'
 
-@app.route('/instance', methods=['GET'])
+@app.route('/instance', methods=['GET', 'POST'])
 def getInstances():
     if request.method == 'GET':
         return get_instances()
+    elif request.method == 'POST':
+        return store_instance()
 
-@app.route('/instance/<instance>', methods=['GET', 'POST', 'DELETE'])
+@app.route('/instance/<instance>', methods=['GET', 'DELETE'])
 def instance(instance):
-    if request.method == 'POST':
-        return store_instance(instance)
-    elif request.method == 'GET':
+    if request.method == 'GET':
         return get_instance_info(instance)
     elif request.method == 'DELETE':
         return delete_instances(instance)
